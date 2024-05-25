ada4e1f2f92467825b0556381a965ba472793865
Dharsan999 <166017224+Dharsan999@users.noreply.github.com>
2024-05-25 15:11:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
         <h1> Hello World </h1>
         <h2> Dharsan </h2>
+    <h2>hi</h2>
       <h2> My name is Abdelali Jadelmoula </h2>
 <h3> I am in love with devops (CI/CD) pipeline </h3>
         </p>
